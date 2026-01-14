4d0b8b05c0451d509d1a58fb76e5d6f1a3f01dc1
Kalasoftware <newsoftwarewala@gmail.com>
2026-01-14 19:15:45 +0530

    Fix ecosystem.config.js syntax

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,16 @@
-{
-  "apps": [{
-    "name": "kaliya-website",
-    "script": "npm",
-    "args": "start",
-    "cwd": "/home/ubuntu/service-kaliya.in",
-    "instances": 1,
-    "autorestart": true,
-    "watch": false,
-    "max_memory_restart": "1G",
-    "env": {
-      "NODE_ENV": "production",
-      "PORT": 9033
+module.exports = {
+  apps: [{
+    name: "kaliya-website",
+    script: "npm",
+    args: "start",
+    cwd: "/home/ubuntu/service-kaliya.in",
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: "1G",
+    env: {
+      NODE_ENV: "production",
+      PORT: 9033
     }
   }]
 }
